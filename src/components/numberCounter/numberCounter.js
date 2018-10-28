0748f910bdf08d0495872241fd2b3b0a2b18ee4d
Deepesh <dbudhia@gmail.com>
2018-10-28 22:30:46 +0800

    Todo: animation turns wrong way when value decreasing

--- src/components/numberCounter/numberCounter.js
+++ src/components/numberCounter/numberCounter.js
@@ -10,9 +10,9 @@ const NumberCounterStyled = styled('div')({
 
 export const NumberCounter = ({ randomNumber }) =>
   <NumberCounterStyled>
-    <div><Odometer start={10500.01} end={1560.05} maxTurnCount={10} theme='dark' prefix='$' animation='turn' duration={2000} /></div>
+    <div><Odometer start={1050.01} end={1560.15} maxTurnCount={10} theme='dark' prefix='$' animation='turn' duration={1000} /></div>
     <br/>
-    <div><Odometer start={10500.01} end={1560.15} maxTurnCount={10} theme='dark' prefix='$' animation='step' duration={2000} /></div>
+    <div><Odometer start={1050.01} end={1560.15} maxTurnCount={10} theme='dark' prefix='$' animation='step' duration={1000} /></div>
   </NumberCounterStyled>
 
 export default connect(({ randNum }) =>
