b9ce0419b698d18ac6d07ab6248a9eecad14a678
Deepesh <dbudhia@gmail.com>
2018-10-29 20:25:34 +0800

    added step values to demo

--- src/components/numberCounter/numberCounter.js
+++ src/components/numberCounter/numberCounter.js
@@ -13,7 +13,11 @@ export const NumberCounter = ({ randomNumber }) =>
     <div><Odometer start={1050.01} end={1560.15} maxTurnCount={10} theme='dark' prefix='$' animation='turn' duration={5000} /></div>
     <br/>
     <div><Odometer start={1560.15} end={1050.01} maxTurnCount={10} theme='dark' prefix='$' animation='turn' duration={5000} /></div>
-
+    <br/>
+    <div><Odometer start={1050.01} end={1560.15} maxTurnCount={100} theme='dark' prefix='$' animation='step' duration={5000} /></div>
+    <br/>
+    <div><Odometer start={1560.15} end={1050.01} maxTurnCount={100} theme='dark' prefix='$' animation='step' duration={5000} /></div>
+    <br/>
   </NumberCounterStyled>
 
 export default connect(({ randNum }) =>
